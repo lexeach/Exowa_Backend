3bc8baf790df9fdb4c6bacadd47bdfd3db5ffbf2
lexeach <80155340+lexeach@users.noreply.github.com>
2025-07-31 14:24:23 +0530

    Update question.ai.js

--- utils/question.ai.js
+++ utils/question.ai.js
@@ -20,7 +20,7 @@ const getGenerateQuestion = async ({
   let lastError = null;
 
   const model = genAI.getGenerativeModel({ 
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
     generationConfig: {
       responseMimeType: "application/json"
     }
