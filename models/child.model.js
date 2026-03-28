56d134825513e2e64ba0605c88c5f4cb2ad9a360
lexeach <80155340+lexeach@users.noreply.github.com>
2026-03-28 16:29:22 +0530

    Update child.model.js

--- models/child.model.js
+++ models/child.model.js
@@ -7,7 +7,7 @@ const childSchema = new mongoose.Schema(
     grade: { type: String, required: true },
     isDeleted: { type: Boolean, default: false }, // Soft delete functionality
     topics: [{ type: String  }],
-    topicLimit: { type: Number, default: 1, min: 0 },
+    topicLimit: { type: Number, default: 3, min: 0 },
     parent: {
       type: String, // References the User model
       required: true, // Optional field to associate with a parent
