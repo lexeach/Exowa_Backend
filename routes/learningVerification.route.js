1882fac6fccc798c2d0540d68aeb745f2b16dbe2
lexeach <80155340+lexeach@users.noreply.github.com>
2026-07-24 16:48:26 +0530

    Update learningVerification.route.js

--- routes/learningVerification.route.js
+++ routes/learningVerification.route.js
@@ -1,15 +1,28 @@
 const express = require("express");
-
 const router = express.Router();
 
-const auth = require("../middlewares/auth.middleware");
+const {
+  auth,
+} = require("../middleware/auth");
 
 const controller = require("../controllers/learningVerification.controller");
 
 router.post(
-    "/generate",
-    auth,
-    controller.generateVerificationPaper
+  "/generate",
+  auth,
+  controller.generateVerification
+);
+
+router.post(
+  "/submit",
+  auth,
+  controller.submitVerification
+);
+
+router.get(
+  "/:paperId/:questionNumber",
+  auth,
+  controller.getVerificationStatus
 );
 
 module.exports = router;
